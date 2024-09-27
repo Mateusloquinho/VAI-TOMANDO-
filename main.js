84348e504f1e29404aaab588aeb77e434cc1d676
Mateusloquinho <143225546+Mateusloquinho@users.noreply.github.com>
2024-09-27 08:54:17 -0300

    .

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-alert('voce clicou no botao');                                                                                                                
+alert ('voce clicou no botao');                                                                                                                
 document.querySelector('button');
-const botao=('button');
-botao.onclick=
-function('voce clicou no botao'){alert
-}
+const botao= document.querySelector('button');
+botao.onclick= function(){
+  alert('voce clicou no botao');
+};
 function soma(num1, num2){
-return num1+num2;
+  return num1+num2;
 }
-console log(soma(5,3));
+console.log(soma(5,3));
