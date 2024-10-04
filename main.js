5a0aa3403a4a9035e8779bc18d0349e315e03810
Mateusloquinho <143225546+Mateusloquinho@users.noreply.github.com>
2024-10-04 08:33:31 -0300

    g

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-alert ('voce clicou no botao');                                                                                                                
-document.querySelector('button');
-const botao= document.querySelector('button');
-botao.onclick= function(){
-  alert('voce clicou no botao');
-};
-function soma(num1, num2){
-  return num1+num2;
-}
-console.log(soma(5,3));
+// alert ('voce clicou no botao');                                                                                                                
+// document.querySelector('button');
+// const botao= document.querySelector('button');
+// botao.onclick= function(){
+//   alert('voce clicou no botao');
+// };
+// function soma(num1, num2){
+//   return num1+num2;
+// }
+// console.log(soma(5,3));
 
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(5));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(5));
 
-let valormedia= parseint(prompt(insira o valor 2);
-let valor= parseint(prompt(insira o valor 3);
-let medial= parseint(prompt(insira o valor 4);
+// let valormedia= parseint(prompt(insira o valor 2);
+// let valor= parseint(prompt(insira o valor 3);
+// let medial= parseint(prompt(insira o valor 4);
 
-let media= function(num2, num3, num4);
+// let media= function(num2, num3, num4);
+
+let estados=['PR','SC','RS','MG','RJ','BA','MS','AM','RR','RO','TO','PI','SE','RN','PE','PB','MT','MS','MA','GO','ES','CE','BSB','AP','AL','AC']
